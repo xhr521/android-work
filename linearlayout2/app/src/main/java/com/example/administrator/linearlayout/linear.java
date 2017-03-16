2e97519295be9633d6487d82a16fd2be0439a43f
xhr521 <364467178@qq.com>
2017-03-16 20:34:14 +0800

    重交

---
+++ linearlayout2/app/src/main/java/com/example/administrator/linearlayout/linear.java
@@ -0,0 +1,13 @@
+package com.example.administrator.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class linear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
