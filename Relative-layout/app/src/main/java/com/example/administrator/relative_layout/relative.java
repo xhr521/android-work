8731687692b45dc2e1353a7470207211c8f87759
xhr521 <364467178@qq.com>
2017-03-16 20:39:38 +0800

    layout

---
+++ Relative-layout/app/src/main/java/com/example/administrator/relative_layout/relative.java
@@ -0,0 +1,13 @@
+package com.example.administrator.relative_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class relative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
